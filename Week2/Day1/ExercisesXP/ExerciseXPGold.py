b287bfb2a12aaa0278d1386a0ed247c4cd54178a
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-03-26 16:18:35 +0300

    xpgold

--- Week2/Day1/ExercisesXP/ExerciseXPGold.py
+++ Week2/Day1/ExercisesXP/ExerciseXPGold.py
@@ -1,51 +1,18 @@
 
-for num in range (0,4):
-    print('Hello world')
-for num in range (0,4):
-    print('I love python')
+print('Hello world\n'*4+'I love Pytnon\n'*4)
+season = input('Imput a number of the month: ')
+num_season = int(season)
+if num_season >= 3 and num_season <= 5:
+    print("It's spring")
 
+if num_season >= 6 and num_season <= 8:
+    print("It's summer")
 
-# num = 99*3/8   
-# print(num)
+if num_season >= 9 and num_season <= 11:
+    print("It's autemn")
 
-# print(5 < 3)
-# print(3 == 3)
-# print(5 == "3")
-# print("Hello" == "hello")
+if num_season >= 1 and num_season <= 2 or num_season == 12:
+    print("It's winter")
 
-# computer_brand = "Dell"
-# print ('I have ' + computer_brand + ' computer')
-
-# name = 'Elena'
-# age = 43
-# shoe_size = 40
-# info = f'Hello, I have a cat his name is {name} he is {age} old and wears shoes of {shoe_size} size' 
-# print (info)
-
-# a = input('First number: ')
-# num_a=int(a)
-# b = input('Second number:')
-# num_b=int(b)
-# if num_a > num_b:
-#     print("hello")
-
-# c = input('Number to define: ')
-# num_c=int(c)
-# d = num_c%2
-# if d == 0:
-#     print("Even")
-# else:
-#     print("odd")
-
-# e = input('What is your name? ')
-# f = "elena"
-# f1 = "Elena"
-# if f == e or f1 == e:
-#     print ("Crazy! We have the same name!")
-# g = input('What is your height in inches? ')
-# num_g = int(g)
-# h = num_g*2.5
-# if h > 145:
-#     print("You're tall enough")
-# else:
-#     print("You need to grow some more to ride")
+if num_season > 12:
+    print("It's not a season, try one more time!")
