07d11b45be0c5ed1682787d7601c11edc3d8e87e
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-03-28 21:41:58 +0300

    some part of work

--- Week2/Day2/DailyChallenge.py
+++ Week2/Day2/DailyChallenge.py
@@ -1,8 +1,16 @@
-number = input("Please, insert a number ")
-number_int=int(number)
-n = 1
-length = input ("Please, insert a length ")
-length_int=int(length)
-for n in range(1, length_int+1):
-    print(number_int*n)
-    n=n+1
+# number = input("Please, insert a number ")
+# number_int=int(number)
+# n = 1
+# length = input ("Please, insert a length ")
+# length_int=int(length)
+# for n in range(1, length_int+1):
+#     print(number_int*n)
+#     n=n+1
+
+word = {}
+doubled_string = input("Please, write a string ")
+doubled_list = list(doubled_string)
+print (doubled_list)
+for n in range(0, len(doubled_string)):
+    if 
+    word = word.add(doubled_list(n))
---
+++ Week2/Day3/DailyChallenge.py
@@ -0,0 +1,22 @@
+inp_string = input("please, write a word: ")
+inp_list = list(inp_string)
+for n in enumerate(inp_string):
+    all_positions = dict(enumerate(inp_string))
+
+
+letter = 
+    if letter in dict
+positions.apend()
+print(all_positions)
+for letter in all_positions:
+    all_positions
+
+# ___________________________________________
+
+items_purchase = {
+  "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
+}
+wallet = "$300"
