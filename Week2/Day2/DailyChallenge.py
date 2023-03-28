a0f4c83e091da552a5f7c5e4026b21ca3a06817e
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-03-28 22:01:33 +0300

    without hidden

--- Week2/Day2/DailyChallenge.py
+++ Week2/Day2/DailyChallenge.py
@@ -1,11 +1,11 @@
-# number = input("Please, insert a number ")
-# number_int=int(number)
-# n = 1
-# length = input ("Please, insert a length ")
-# length_int=int(length)
-# for n in range(1, length_int+1):
-#     print(number_int*n)
-#     n=n+1
+number = input("Please, insert a number ")
+number_int=int(number)
+n = 1
+length = input ("Please, insert a length ")
+length_int=int(length)
+for n in range(1, length_int+1):
+    print(number_int*n)
+    n=n+1
 
 word = {}
 doubled_string = input("Please, write a string ")
@@ -14,3 +14,4 @@ print (doubled_list)
 for n in range(0, len(doubled_string)):
     if 
     word = word.add(doubled_list(n))
+# here i was lost
