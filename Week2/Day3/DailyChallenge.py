9e4c9641f706b6cb7157b98edc1a01e4f8bc3ba7
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-03-29 09:35:48 +0300

    partly daily

--- Week2/Day3/DailyChallenge.py
+++ Week2/Day3/DailyChallenge.py
@@ -1,22 +1,21 @@
-inp_string = input("please, write a word: ")
-inp_list = list(inp_string)
-for n in enumerate(inp_string):
-    all_positions = dict(enumerate(inp_string))
+word_inp = input("please, write a word: ")
+final_list = {}
+i=0
+for n in word_inp:
+    if n not in final_list.keys():
+        final_list[n]= [i]
+    else:
+        final_list[n].append(i)
+    i +=1
+print (final_list)
 
-
-letter = 
-    if letter in dict
-positions.apend()
-print(all_positions)
-for letter in all_positions:
-    all_positions
 
 # ___________________________________________
 
-items_purchase = {
-  "Water": "$1",
-  "Bread": "$3",
-  "TV": "$1,000",
-  "Fertilizer": "$20"
-}
-wallet = "$300"
+# items_purchase = {
+#   "Water": "$1",
+#   "Bread": "$3",
+#   "TV": "$1,000",
+#   "Fertilizer": "$20"
+# }
+# wallet = "$300"
--- Week2/Day3/ExercisesXP.py
+++ Week2/Day3/ExercisesXP.py
@@ -1,86 +1,86 @@
-# keys = ['Ten', 'Twenty', 'Thirty']
-# values = [10, 20, 30]
-# dictin1 = dict(zip(keys, values))
-# print (dictin1)
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dictin1 = dict(zip(keys, values))
+print (dictin1)
 
 
-# family = {"Rick": 43, 'Beth': 13, 'Morty': 5, 'Summer': 8}
-# total_cost = 0
-# list_names = dict.keys(family)
-# print(list_names)
-# for name in list_names:
-#     if family[name]>3 and family[name]<12:
-#         price = 10
-#         print(name,":", price, "$") 
-#         total_cost = total_cost+10
-#     if family[name] >= 13:
-#         price = 15
-#         print(name, ":", price, "$") 
-#         total_cost = total_cost+10
-#     if family[name] < 3: 
-#         print(name, ": free of charge")
-#         total_cost = total_cost
-# print("Total sum equals:", total_cost, "$")
-# # ====================BONUS=============
+family = {"Rick": 43, 'Beth': 13, 'Morty': 5, 'Summer': 8}
+total_cost = 0
+list_names = dict.keys(family)
+print(list_names)
+for name in list_names:
+    if family[name]>3 and family[name]<12:
+        price = 10
+        print(name,":", price, "$") 
+        total_cost = total_cost+10
+    if family[name] >= 13:
+        price = 15
+        print(name, ":", price, "$") 
+        total_cost = total_cost+10
+    if family[name] < 3: 
+        print(name, ": free of charge")
+        total_cost = total_cost
+print("Total sum equals:", total_cost, "$")
+# ====================BONUS=============
 
-# family_unknown = {}
-# family = {"Rick": 43, 'Beth': 13, 'Morty': 5, 'Summer': 8}
-# total_cost = 0
-# list_names = dict.keys(family)
-# print(list_names)
-# for name in list_names:
-#     if family[name]>3 and family[name]<12:
-#         price = 10
-#         print(name,":", price, "$") 
-#         total_cost = total_cost+10
-#     if family[name] >= 13:
-#         price = 15
-#         print(name, ":", price, "$") 
-#         total_cost = total_cost+10
-#     if family[name] < 3: 
-#         print(name, ": free of charge")
-#         total_cost = total_cost
-# print("Total sum equals:", total_cost, "$")
+family_unknown = {}
+family = {"Rick": 43, 'Beth': 13, 'Morty': 5, 'Summer': 8}
+total_cost = 0
+list_names = dict.keys(family)
+print(list_names)
+for name in list_names:
+    if family[name]>3 and family[name]<12:
+        price = 10
+        print(name,":", price, "$") 
+        total_cost = total_cost+10
+    if family[name] >= 13:
+        price = 15
+        print(name, ":", price, "$") 
+        total_cost = total_cost+10
+    if family[name] < 3: 
+        print(name, ": free of charge")
+        total_cost = total_cost
+print("Total sum equals:", total_cost, "$")
 
 
-# # ====================ZARA=============
+# ====================ZARA=============
 
-# brand = {"name": "Zara",
-#         "creation_date": "1975",
-#         "creator_name": "Amancio Ortega Gaona",
-#         "type_of_clothes": ["men", "women", "children", "home"], 
-#         "international_competitors": ["Gap", "H&M", "Benetton"],
-#         "number_stores": "7000",
-#         "major_color":  {
-#             "France": "blue", 
-#             "Spain": "red", 
-#             "US": ["pink", "green"]}
-#             }
-# print (brand)
-# brand["number_stores"] = 2
-# print(brand)
-# brand['clients'] = "everybody"
-# print(brand)
-# brand['country_creation'] = "Spain"
-# print(brand)
-# list_names = dict.keys(brand)
-# a = "international_competitors"
-# if a in list_names:
-#     brand["international_competitors"].append("Desigual")
-# print(brand)
-# del brand["creation_date"]
-# print(brand)
-# print(brand["international_competitors"][-1])
-# print(brand["major_color"]["US"])
-# print(len(brand))
-# print(brand.keys())
-# more_on_zara = {"creation_date": "1975",
-#                 "number_stores": "10000"}
-# brand.update(more_on_zara)
+brand = {"name": "Zara",
+        "creation_date": "1975",
+        "creator_name": "Amancio Ortega Gaona",
+        "type_of_clothes": ["men", "women", "children", "home"], 
+        "international_competitors": ["Gap", "H&M", "Benetton"],
+        "number_stores": "7000",
+        "major_color":  {
+            "France": "blue", 
+            "Spain": "red", 
+            "US": ["pink", "green"]}
+            }
+print (brand)
+brand["number_stores"] = 2
+print(brand)
+brand['clients'] = "everybody"
+print(brand)
+brand['country_creation'] = "Spain"
+print(brand)
+list_names = dict.keys(brand)
+a = "international_competitors"
+if a in list_names:
+    brand["international_competitors"].append("Desigual")
+print(brand)
+del brand["creation_date"]
+print(brand)
+print(brand["international_competitors"][-1])
+print(brand["major_color"]["US"])
+print(len(brand))
+print(brand.keys())
+more_on_zara = {"creation_date": "1975",
+                "number_stores": "10000"}
+brand.update(more_on_zara)
 
-# print("Updated info:", brand)
-# print(brand["number_stores"]) 
-# # info rewritten
+print("Updated info:", brand)
+print(brand["number_stores"]) 
+# info rewritten
 
 # # --------------------Exercise 4---------------
 users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
