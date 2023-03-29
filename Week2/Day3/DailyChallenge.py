06a61f90d1d42b07611f23a7e24c0cb8ac7217ab
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-03-29 09:58:43 +0300

    second version

--- Week2/Day3/DailyChallenge.py
+++ Week2/Day3/DailyChallenge.py
@@ -1,21 +1,40 @@
-word_inp = input("please, write a word: ")
-final_list = {}
-i=0
-for n in word_inp:
-    if n not in final_list.keys():
-        final_list[n]= [i]
-    else:
-        final_list[n].append(i)
-    i +=1
-print (final_list)
+# word_inp = input("please, write a word: ")
+# final_list = {}
+# i=0
+# for n in word_inp:
+#     if n not in final_list.keys():
+#         final_list[n]= [i]
+#     else:
+#         final_list[n].append(i)
+#     i +=1
+# print (final_list)
 
 
 # ___________________________________________
 
-# items_purchase = {
-#   "Water": "$1",
-#   "Bread": "$3",
-#   "TV": "$1,000",
-#   "Fertilizer": "$20"
-# }
-# wallet = "$300"
+# word_inp1 = input("please, write a word: ")
+# my_seq = defaultseq(list)
+
+# for index, letter in enumerate(word_inp1):
+#     my_seq[letter].add(index)
+# print (my_seq)
+
+# ______________
+# word_inp2 = input("please, write a word: ")
+# final_list3 = {}
+# for q, latter in enumerate(word_inp2):
+#     if latter in final_list3:
+#         final_list3[latter].append(q)
+#     else:
+#         final_list3[latter] = [q]
+# print (final_list3)
+
+# =======================
+items_purchase = {
+    "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
+}
+wallet = "$300"
+
