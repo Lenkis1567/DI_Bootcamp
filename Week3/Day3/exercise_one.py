e2bf26c54c08c226c2561d5e37ef993632e70511
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-04-14 16:13:56 +0300

    exerciseXP modules

--- Week3/Day3/exercise_one.py
+++ Week3/Day3/exercise_one.py
@@ -44,4 +44,38 @@ def life_days():
 
 life_days()
 # =================Upcoming Holiday===============================
-# ====================================================================
+def holiday_time():
+    import datetime
+    today = datetime.datetime.today()
+    print(today)
+    holiday=datetime.datetime(2023,4,26,0,0,0)
+    time_till_holiday=holiday-today
+    str=f"Till holiday {time_till_holiday}"
+    print(str[0:27], "min")
+holiday_time()
+# =======================Exercise 8 : How Old Are You On Jupiter?================
+def age_anotherplanet(age, planet):
+    earth=31557600
+    koeff={"earth":31557600, "mercury":earth*0.2408467,"venus": earth*0.61519726, "mars":earth*1.8808158, "jupiter": earth*11.862615,"saturn": earth*29.447498,"uranus":earth*84.016846, "neptune":earth*164.79132}
+    
+    return round(age/koeff[planet], 2)
+age=int(input("Input your age in seconds: "))
+planet=input("Input the planet you wanna live: ")
+print(age_anotherplanet(age,planet))
+# ======================Exercise 9 : Faker Module=========
+from faker import Faker
+fake = Faker()
+users=[]
+user={}
+quantity=int(input("How many people do you need? "))
+for i in range (0,quantity):
+    user["Name"]= fake.unique.name()
+    user["Address"]=fake.address()
+    user["Langage code"]="eng"
+    users.append(user)
+    user={}
+print(users)
+
+
+
+
