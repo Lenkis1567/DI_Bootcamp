117a30ca6cb8d16cc8cb0bbbae39edee607865f6
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-05-16 17:55:41 +0300

    JavaDaily2

--- Week8JavaS/Day1/javaD.js
+++ Week8JavaS/Day1/javaD.js
@@ -1,14 +1,28 @@
-let sentence="The weather is not as bad as I expected"
-let search_word ='not'
-let search_word1 = 'bad'
-words = sentence.split(" ")
-let wordNot = words.indexOf(search_word.toLowerCase());
-let wordBad = words.indexOf(search_word1.toLowerCase());
-console.log(wordNot, wordBad)
+// let sentence="The weather is not as bad as I expected"
+// let search_word ='not'
+// let search_word1 = 'bad'
+// words = sentence.split(" ")
+// let wordNot = words.indexOf(search_word.toLowerCase());
+// let wordBad = words.indexOf(search_word1.toLowerCase());
+// console.log(wordNot, wordBad)
 
-replace_str= 'good'
-if (wordBad>wordNot) {
-    words.splice(wordNot, wordBad - wordNot + 1, replace_str);
+// replace_str= 'good'
+// if (wordBad>wordNot) {
+//     words.splice(wordNot, wordBad - wordNot + 1, replace_str);
+// }
+// let result= words.join(" ")
+// console.log(result)
+
+let zv='*'
+for (let i=1; i<7; i++){
+    zv=""
+    for (let y=1; y<i; y++) {
+    zv=zv+'*'
+    };
+    console.log(zv)
 }
-let result= words.join(" ")
-console.log(result)
+
+let zv1='*'
+for (let a=1; a<6; a++) {
+    console.log(zv1.repeat(a))
+}
