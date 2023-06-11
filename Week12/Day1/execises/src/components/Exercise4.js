d258f62ff82ca92361d278b476c7e25661dfbcdd
Lenkis1567 <chuvilinaelena.manager@gmail.com>
2023-06-11 17:24:05 +0300

    exercise plus form

--- Week12/Day1/execises/src/components/Exercise4.js
+++ Week12/Day1/execises/src/components/Exercise4.js
@@ -11,6 +11,17 @@ return (
     <div> 
         <h1 style={style_header}>Cool header</h1>
         <p className="para">Paragraph</p>
+        <form>
+            <label> Name:
+            <input type="text" />
+            </label>
+            <input type="submit" value="Submit" />
+      </form>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
         <img src='/orch.jpg' alt='Orchid' />
         <a href ="https://en.wikipedia.org/wiki/Orchid">Orchids</a>
     </div>
